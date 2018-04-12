53c64c8a9b3e036b60fc9ece22049e10f6556f4e
Alvin Le <alvin.le@workday.com>
2018-04-12 11:33:53 -0700

    Added closing of Selenium driver, small improvements

--- README.md
+++ README.md
@@ -4,4 +4,4 @@ Selenium script that checks if a class at Diablo Valley College has a seat avail
 ## Setup
 > Create a 'creds', 'data', and 'logs' folder inside the root directory
 
-> Inside the 'creds' directory, create a 'creds.txt' with the Twilio credentials and a 'phone_numbers.txt' with the recipient's phone number on the first line and the Twilio number on the second
+> Inside the 'creds' directory, create a 'creds.txt' with the Twilio Account SID on the first line and the Twilio Auth_Token on the second, and a 'phone_numbers.txt' with the recipient's phone number on the first line and the Twilio number on the second
--- main.py
+++ main.py
@@ -1,26 +1,25 @@
 from dvcs_client import DvcsClient
 import os
-import json
 import logging
 import traceback
-
 
 # Logger
 directory = os.path.dirname(os.path.abspath(__file__))
 log_file = os.path.join(directory, './logs/activity.log')
-logging.basicConfig(filename=log_file, level=logging.INFO, format='%(asctime)s %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
+logging.basicConfig(filename=log_file, level=logging.INFO, format='%(asctime)s %(message)s',
+                    datefmt='%Y-%m-%d %H:%M:%S')
 logger = logging.getLogger(__name__)
 
 DvcsClient = DvcsClient()
 
 
 def main():
-
-	try:
-		DvcsClient.check_availability('+15551234567', 'DVC', '2018SU', 'COMSC-165')
-	except Exception as e:
-		logging.warning(str(traceback.format_exc()))
+    try:
+        DvcsClient.check_availability('+15551234567', 'DVC', '2018SU', 'COMSC-200')
+    except Exception as e:
+        logging.warning(str(traceback.format_exc()))
+        raise e
 
 
 if __name__ == '__main__':
-	main()
+    main()
